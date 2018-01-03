c0eb55aeee0fdf70d92633a827b55edbfc5caf0b
Guilherme Eduardo <guilherme.eduardo92@hotmail.com>
2018-01-03 17:57:16 -0200

    Added Router

--- src/index.js
+++ src/index.js
@@ -2,14 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import App from './components/app';
+import Home from './components/home';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />>
+      </Switch>
+    </Router>
   </Provider>
   , document.getElementById('root'));
